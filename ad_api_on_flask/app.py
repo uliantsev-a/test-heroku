2ebe9ee39ff36085b8fa401626ed52856ac40a70
andrey <andrey_belyanin@mail.ru>
2021-07-07 19:27:56 +0200

    port added

--- ad_api_on_flask/app.py
+++ ad_api_on_flask/app.py
@@ -221,4 +221,5 @@ def ad_del(id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.getenv('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
