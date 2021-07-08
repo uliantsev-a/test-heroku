92e2d6f1c6162c4a2806b5f9df44edeb0c33f0fc
andrey32741553 <65930776+andrey32741553@users.noreply.github.com>
2021-07-08 21:26:09 +0200

    Update app.py

--- ad_api_on_flask/app.py
+++ ad_api_on_flask/app.py
@@ -11,6 +11,8 @@ from schema import AD_CREATE, USER_CREATE
 
 app = Flask(__name__)
 jwt = JWTManager(app)
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{}:{}@localhost:5432/ad_api'.format(os.getenv('DB_USER'),
                                                                                           os.getenv('DB_PASSWORD'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
